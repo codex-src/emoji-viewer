cd54f6c1c97f9ef36a101837ff627cc50e7b77e0
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2020-03-31 23:42:38 +0900

    Heavily filtered shown emojis and added a sticky search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
 import React from "react"
+import $dataset from "./UNICODE_13-0_DO-NOT-EDIT.generated.json"
 
-const App = props => (
-	"hello, world!"
-)
+// TODO: Precompute
+const components = $dataset
+	.filter(each => each.status === "component")
+	.map(each => each.codePoints[0]) // Assumes component emojis are one code point
+
+// TODO: Precompute
+const relevant = $dataset
+	// .slice(0, 32)
+	.filter(each => {
+		// Status:
+		if (each.status !== "fully-qualified") {
+			return false
+		}
+		// Version:
+		const { tag } = each
+		const version = +tag.slice(1) // Coerce to a number
+		// Minimum supported version:
+		if (version >= 12) {
+			return false
+		}
+		// Component:
+		const { codePoints } = each
+		const hasComponent = codePoints.some(codePoint => components.includes(codePoint))
+		if (hasComponent) {
+			return false
+		}
+		return true
+	})
+
+const App = props => {
+	const [query, setQuery] = React.useState("")
+
+	return (
+		<div className="py-32 flex flex-row justify-center" style={{ fontFamily: "'BlinkMacSystemFont', 'Apple Color Emoji'" }}>
+			<div className="px-6 w-full max-w-screen-lg">
+
+				{/* Search */}
+				<div className="-mt-8 pt-8 sticky top-0 z-40">
+					<div className="px-6 h-16 bg-white rounded-lg shadow-hero-lg overflow-none">
+						<input className="w-full h-full bg-transparent outline-none" type="text" value={query} onChange={e => setQuery(e.target.value)} />
+					</div>
+				</div>
+
+				{/* Grid */}
+				<div className="h-8" />
+				<div className="grid grid-cols-8 gap-3">
+					{relevant.map(each => (
+						<div key={each.codePoints.join("-")} className="pb-1/1 relative">
+							<div className="absolute inset-0">
+								<div className="flex flex-row justify-center items-center h-full">
+									<span style={{ fontSize: "3.25em" }} data-description={each.description}>
+										{each.emoji}
+									</span>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import App from "./App"
 import React from "react"
 import ReactDOM from "react-dom"
 
+import "./index.css"
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
